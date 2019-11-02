6b5a369ac47380cf43017ec3ab329d5a4e26c7c5
Rashpal Gill <rsgillit@gmail.com>
2019-11-02 15:23:16 -0500

    dependency injection demo

---
+++ springframework-didemo/src/main/java/com/gill/springframeworkdidemo/SpringframeworkDidemoApplication.java
@@ -0,0 +1,13 @@
+package com.gill.springframeworkdidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringframeworkDidemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringframeworkDidemoApplication.class, args);
+    }
+
+}
---
+++ springframework-didemo/src/test/java/com/gill/springframeworkdidemo/SpringframeworkDidemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gill.springframeworkdidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringframeworkDidemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
