38405e1ed59d6d5f0d7def570e1dbcc08e1313f4
Rashpal Gill <rsgillit@gmail.com>
2019-11-11 12:32:25 -0600

    added xml config

--- springboot-jokes-app/src/main/java/com/gill/springbootjokesapp/SpringbootJokesAppApplication.java
+++ springboot-jokes-app/src/main/java/com/gill/springbootjokesapp/SpringbootJokesAppApplication.java
@@ -2,8 +2,11 @@ package com.gill.springbootjokesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class SpringbootJokesAppApplication {
 
     public static void main(String[] args) {
